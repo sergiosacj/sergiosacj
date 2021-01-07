7f5790c2121b7a9e4190608645fcd725c22238b1
Sergio de Almeida Cipriano Junior <sergiosacj@hotmail.com.br>
2021-01-07 10:25:12 -0300

    Start navbar development

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import "./index.scss";
+import logo from "./assets/logo.svg";
+
+const Navbar: React.FunctionComponent = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img
+            src={logo}
+            alt="logo"
+            width="100%"
+            height="20px"
+          />
+        </div>
+        <ul className={`menu`}>
+          <li>home</li>
+          <li>about</li>
+          <li>project</li>
+          <li>blog</li>
+          <li>contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
