0cb66a36ca2ad5680037528dc9145c38613c074d
Sergio de Almeida Cipriano Junior <sergiosacj@hotmail.com.br>
2021-01-07 17:01:15 -0300

    navbar: adding fabar for when the screen is small

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,42 @@
 import * as React from "react";
 import "./index.scss";
 import logo from "./assets/logo.svg";
+import { FaBars } from "react-icons/fa";
 
 const Navbar: React.FunctionComponent = () => {
+  const [showSidebar, setShowSidebar] = React.useState(false);
+
+  const checkWindowSize = () => {
+    window.innerWidth < 900
+      ? setShowSidebar(true)
+      : setShowSidebar(false);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("resize", checkWindowSize);
+    return () =>
+      window.removeEventListener("resize", checkWindowSize);
+  }, []);
+
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
           <img src={logo} alt="logo" />
         </div>
-        <ul className={`menu`}>
-          <li>home</li>
-          <li>about</li>
-          <li>project</li>
-          <li>blog</li>
-          <li>contact</li>
-        </ul>
+        {!showSidebar ? (
+          <ul className="menu">
+            <li>home</li>
+            <li>about</li>
+            <li>project</li>
+            <li>blog</li>
+            <li>contact</li>
+          </ul>
+        ) : (
+          <div className="fabar-btn">
+            <FaBars />
+          </div>
+        )}
       </div>
     </div>
   );
