f943a7a363db69ab03d244bceb4fb2770de6b391
Sergio de Almeida Cipriano Junior <sergiosacj@hotmail.com.br>
2021-01-10 19:46:29 -0300

    hero: Set better position on mobile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,11 @@ import * as React from "react";
 import { Hero } from "../../components";
 
 const Home: React.FC = () => {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
