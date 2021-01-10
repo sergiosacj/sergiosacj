ae7d85e142f6ece73034d9e8a7ac3119ce5e0be6
Sergio de Almeida Cipriano Junior <sergiosacj@hotmail.com.br>
2021-01-10 19:26:32 -0300

    Create About page

---
+++ src/pages/About/_knowledge.tsx
@@ -0,0 +1,13 @@
+export const knowledges = [
+  "html",
+  "css",
+  "javascript",
+  "typescript",
+  "python",
+  "julia",
+  "C",
+  "C++",
+  "reactjs",
+  "docker",
+  "GNU/Linux",
+];
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,7 +1,31 @@
 import * as React from "react";
+import { knowledges } from "./_knowledge";
+import "./index.scss";
 
 const About: React.FC = () => {
-  return <>About</>;
+  return (
+    <>
+      <div className="container">
+        <div className="about-me">
+          <h1>about me</h1>
+          <div className="underline"></div>
+          <p className="description">
+            Hello, I am Sérgio Cipriano, software engineering
+            student from UnB. I currently study optimization,
+            supervised by John Lenon Cardoso Gardenghi. I spend
+            most of my day contributing to free software,
+            studying new languages, frameworks and creating new
+            ones too.
+          </p>
+          <div className="knowledge">
+            {knowledges.map((name, index) => {
+              return <span key={index}>{name}</span>;
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
