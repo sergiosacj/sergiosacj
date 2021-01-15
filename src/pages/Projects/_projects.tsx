aee52761605a88cd2da781931c2ecf556e848a7d
Sergio de Almeida Cipriano Junior <sergiosacj@hotmail.com.br>
2021-01-15 18:30:14 -0300

    projects: first version of ugly cards

--- src/pages/Projects/_projects.tsx
+++ src/pages/Projects/_projects.tsx
@@ -1,9 +1,25 @@
 export const projects = [
   {
     name: "ArBC",
-    source: "https://github.com/sergiosacj/2019.2-ArBC",
+    source: "https://github.com/sergiosacj/",
+    technologies: ["Vuejs", "Django", "Postgres"],
+    description:
+      "ArBC is a web application based on AR.js and A-Frame AR (Augmented Reality) APIs, which aims to make the literacy process more interactive and engaging through this technology",
+  },
+  {
+    name:
+      "topics in nonlinear programming and computational linear algebra",
+    source: "https://github.com/sergiosacj/IC-2019-2020",
+    technologies: ["Julia", "Jupyter Notebook"],
+    description:
+      "This work addresses the method of conjugated gradients for solving a subproblem of the Newton method for unrestricted nonlinear systems",
+  },
+  {
+    name: "Debian QA",
+    source:
+      "https://qa.debian.org/developer.php?login=sergiosacj%40hotmail.com.br&comaint=yes",
     technologies: [],
     description:
-      "ArBC is a web application based on AR.js and A-Frame AR (Augmented Reality) APIs, which aims to make the literacy process more interactive and engaging through this technology.",
+      "I started contributing on 2020 and I have been working on quality assurance. I am currently a active member on Debian Brasília team",
   },
 ];
--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -6,18 +6,33 @@ const Projects: React.FC = () => {
   return (
     <div className="container">
       <div className="projects-page">
-        <h1>projects</h1>
+        <h1>my projects</h1>
         <div className="underline"></div>
         <div className="projects">
-          {projects.map((project) => {
-            const {
-              name,
-              source,
-              technologies,
-              description,
-            } = project;
-            return <article></article>;
-          })}
+          <div className="projects-container">
+            {projects.map((project) => {
+              const {
+                name,
+                source,
+                technologies,
+                description,
+              } = project;
+              return (
+                <article>
+                  <h3>{name}</h3>
+                  <p>{description.slice(0, 200)}...</p>
+                  <ul>
+                    {technologies.map((item, index) => {
+                      return <li key={index}>{item}</li>;
+                    })}
+                  </ul>
+                  <div className="links">
+                    <a href={source}>source code</a>
+                  </div>
+                </article>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
